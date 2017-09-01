5394c7f59bfa3fff298e3289aa82af50f82f2165
Sagarnath Konda <mail2reachsagar@gmail.com>
2017-09-01 18:55:18 -0500

    part1/section2/src/pack2/B2.java

--- part1/section2/src/pack2/B2.java
+++ part1/section2/src/pack2/B2.java
@@ -1,13 +1,15 @@
 package pack2;
 class B1 
 {
-	static int i=20;
+	static int i=50;
+	static double j=99.99
 }
 class B2
 {
 	public static void main(String a[])
 	{
 		System.out.println(B1.i);
+		System.out.println(B1.j);
 		System.out.println(pack1.B1.i);
 	}
 }
